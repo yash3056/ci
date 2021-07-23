3acd307998c1a072d11ad67c6dfd3018dbac0fe0
yash <73551736+yash3056@users.noreply.github.com>
2021-07-23 17:38:45 +0530

    disable notifcation

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/yash3056/android_kernel_xiaomi_vayu.git -b r1 kernel
+git clone --depth=1 https://github.com/vayu-lab/android_kernel_xiaomi_vayu -b sleepy kernel --depth=1
 git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 git clone --depth=1 https://github.com/dracarys18/AnyKernel3 AnyKernel
 echo "Done"
@@ -15,37 +15,37 @@ export ARCH=arm64
 export KBUILD_BUILD_HOST=notkernel
 export KBUILD_BUILD_USER="JonSnow"
 # sticker plox
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAADBQADVAADaEQ4KS3kDsr-OWAUFgQ" \
-        -d chat_id=$chat_id
+#function sticker() {
+#    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
+#        -d sticker="CAADBQADVAADaEQ4KS3kDsr-OWAUFgQ" \
+#        -d chat_id=$chat_id
 }
 # Send info plox channel
-function sendinfo() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>• NotKernel •</b>%0ABuild started on <code>Circle CI/CD</code>%0A <b>For device</b> <i>Xiaomi Redmi K20 Pro (vayu)</i>%0A<b>branch:-</b> <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0A<b>Under commit</b> <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0A<b>Using compiler:- </b> <code>$CLANG_VERSION</code>%0A<b>Started on:- </b> <code>$(date)</code>%0A<b>Build Status:</b> #Test"
+#function sendinfo() {
+#    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+#        -d chat_id="$chat_id" \
+#        -d "disable_web_page_preview=true" \
+#        -d "parse_mode=html" \
+#        -d text="<b>• NotKernel •</b>%0ABuild started on <code>Circle CI/CD</code>%0A <b>For device</b> <i>Xiaomi Redmi K20 Pro (vayu)</i>%0A<b>branch:-</b> <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0A<b>Under commit</b> <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0A<b>Using compiler:- </b> <code>$CLANG_VERSION</code>%0A<b>Started on:- </b> <code>$(date)</code>%0A<b>Build Status:</b> #Test"
 }
 # Push kernel to channel
-function push() {
-    cd AnyKernel
-    ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi K20Pro (vayu)</b> | <b>$CLANG_VERSION</b>"
+#function push() {
+#    cd AnyKernel
+#    ZIP=$(echo *.zip)
+#    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+#        -F chat_id="$chat_id" \
+#        -F "disable_web_page_preview=true" \
+#        -F "parse_mode=html" \
+#        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi K20Pro (vayu)</b> | <b>$CLANG_VERSION</b>"
 }
 # Fin Error
-function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d text="Build throw an error(s)"
-    exit 1
+#function finerr() {
+#    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+#        -d chat_id="$chat_id" \
+#        -d "disable_web_page_preview=true" \
+#        -d "parse_mode=markdown" \
+#        -d text="Build throw an error(s)"
+#    exit 1
 }
 # Compile plox
 function compile() {
